392c658176a9f691faf89898a172ed5b4d3f7bd6
MugiwaraDR <ramonsoares10@outlook.com>
2020-06-02 19:55:29 -0300

    primeiro commit

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import knex from './database/connection';
+
+const routes = express.Router();
+
+routes.get('/items', async (request , response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return 1
+    });
+
+    return response.json(serializedItems);
+});
+
+ export default routes;
